3cdd5e5ef5bba4a1e471cce870c568174e178667
Debid Magar <linkwithdm@gmail.com>
2022-01-30 23:20:58 +0545

    remoted db added

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const cors = require("cors");
-const errorHandler = require("./src/helpers/errorHandler");
-require("dotenv").config();
+const cors = require('cors');
+const errorHandler = require('./src/helpers/errorHandler');
+require('dotenv').config();
 app.use(cors());
 app.use(
   express.urlencoded({
@@ -13,12 +13,23 @@ app.use(
 
 app.use(express.json());
 
-require("./src/helpers/db");
+require('./src/helpers/db');
 
 const api = process.env.API;
-app.use(`${api}/`, require("./src/routes/api.routes"));
+app.use(`${api}/`, require('./src/routes/api.routes'));
 
 app.use(errorHandler);
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+// //Production server
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, (err, done) => {
+  if (err) {
+    console.log('Error while listening port ' + app.get('port') + ' >> ' + err);
+  } else {
+    console.log('Server is listening at port ' + app.get('port'));
+  }
 });
